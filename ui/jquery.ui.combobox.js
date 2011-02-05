33f80f2f69e562b90792ced88859634649e2afa4
glebm <glex.spb@gmail.com>
2011-02-05 10:47:01 +0100

    Combobox performance optimizations

--- ui/jquery.ui.combobox.js
+++ ui/jquery.ui.combobox.js
@@ -1,32 +1,39 @@
-(function($) {
+(function($, undefined) {
+
     $.widget("ui.combobox", {
         options: {
             dataFilter: null,
+
+            minLength: 1,
 
             data: function(ui) {
                 ui.data = ui.data || ui.element.find("option").map(function() {
                     if (!this.value) {
                         return null;
                     }
-                    var text   = this.innerText || this.textContent,
-                        parent = this.parentNode,
-                        result = {
-                            text   : text       ,
-                            trimmed: text.trim(),
-                            option : this
-                        };
+                    var text = this.innerText || this.textContent,
+                            parent = this.parentNode,
+                            result = {
+                                text   : text       ,
+                                trimmed: text.trim(),
+                                value  : this.value
+                            };
 
                     if (parent && parent.nodeType === 1 && parent.nodeName.toLowerCase() == "optgroup") {
                         result.group = parent.label;
                     }
-                    
+
                     return result;
                 });
                 return ui.data;
             },
 
-            highlight: function(text, escapedTerm){
-                return text.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + escapedTerm + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>");
+
+            highlight: function(text, escapedTerm) {
+                if (!escapedTerm) {
+                    return text;
+                }
+                return text.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + escapedTerm + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<b>$1</b>");
             },
 
             placeholder: "Type to search...",
@@ -52,6 +59,11 @@
                     .insertAfter($select)
                     .val(value);
 
+            var optionByValue = {};
+            $select.find('option').each(function() {
+               optionByValue[this.value] = this;
+            });
+
             var filter = options.dataFilter,
                     placeholder = $select.attr('data-placeholder') || options['placeholder'];
 
@@ -59,12 +71,12 @@
 
 
             if (options.preventFormSubmit) {
-                $input.keydown(function(event){
-                   if (event.keyCode == 13) {
-                       event.preventDefault();
-                       $input.autocomplete('option', 'change').call($input, event, {});
-                       $input.autocomplete('close')
-                   }
+                $input.keydown(function(event) {
+                    if (event.keyCode == 13) {
+                        event.preventDefault();
+                        $input.autocomplete('option', 'change').call($input, event, {});
+                        $input.autocomplete('close')
+                    }
                 });
             }
 
@@ -74,16 +86,20 @@
 
                 // Show only matched items
                 source: function(request, response) {
+                    if (request.term && request.term.length < options.minLength) {
+                        response([]);
+                        return;
+                    }
                     var termForRegex = $.ui.autocomplete.escapeRegex(request.term),
                             matcher = new RegExp(termForRegex, "i");
-                    self._trigger("search", event, request.term);
+                    self._trigger("search", null, request.term);
                     response(data(self).map(function() {
                         if (this && (!request.term || matcher.test(this.trimmed)) && !(filter && filter(this)))
                             return {
                                 label: options.highlight(this.text, termForRegex),
                                 value: this.trimmed,
                                 group: this.group,
-                                option: this.option
+                                optionValue: this.value
                             };
                     }));
                 },
@@ -97,10 +113,12 @@
                     self._trigger("search", event, $(this).val());
                     data(self).each(function() {
                         if (matcher.test(this.trimmed) && !(filter && filter(this))) {
-                            this.option.selected = valid = true;
+                            var option = optionByValue[this.value];
+                            option.selected = valid = true;
                             self._trigger("selected", event, {
-                                item: this.option
+                                item: option
                             });
+                            $select.trigger("change");
                             return false;
                         }
                     });
@@ -114,9 +132,11 @@
 
                 select: function(event, ui) {
                     // Clear the box if not item was selected and the value typed in does not match anything
-                    ui.item.option.selected = true;
+                    var option = optionByValue[ui.item.optionValue];
+                    option.selected = true;
+                    $select.trigger("change");
                     return self._trigger("selected", event, {
-                        item: ui.item.option
+                        item: option
                     });
                 }
             });
@@ -124,21 +144,22 @@
             $input.addClass("ui-widget ui-widget-content ui-corner-left");
 
             $input.data("autocomplete")._renderMenu = function(ul, items) {
-                var self = this, currentGroup = "";
+                var currentGroup = "", listElements = [];
                 $.each(items, function(index, item) {
                     if (item.group && item.group != currentGroup) {
-                        ul.append("<li class='ui-combobox-group'>" + item.group + "</li>");
+                        listElements[listElements.length] = "<li class='ui-combobox-group'>" + $.ui.combobox.escapeHTML(item.group) + "</li>";
                         currentGroup = item.group;
                     }
-                    self._renderItem(ul, item);
-                });
-            };
 
-            $input.data("autocomplete")._renderItem = function(ul, item) {
-                return $("<li></li>")
-                        .data("item.autocomplete", item)
-                        .append("<a>" + item.label + "</a>")
-                        .appendTo(ul);
+                    listElements[listElements.length] = "<li data-item.autocomplete='" +
+                            JSON.stringify(item).replace(/'/g, '&#39;') + "'><a>" + item.label + "</a></li>";
+                });
+
+                ul[0].innerHTML = listElements.join("");
+
+//                ul.find("li:not(.ui-combobox-group)").each(function(i) {
+//                   $(this).data("item.autocomplete", items[i]);
+//                });
             };
 
             this.button = $("<button type='button' class='combobox'>&nbsp;</button>")
@@ -171,6 +192,13 @@
             this.button.remove();
             this.element.show();
             $.Widget.prototype.destroy.call(this);
+        }
+    });
+
+    $.extend($.ui.combobox, {
+        version: "@VERSION",
+        escapeHTML: function(value) {
+            return value.replace(/&/g, '&amp;').replace(/>/g, '&gt;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
         }
     });
 })(jQuery);
